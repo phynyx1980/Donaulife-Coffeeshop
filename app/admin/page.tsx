331bed28381a1214711ab579cca78cde48bad41f
Phynyx Trust <phynyx1980@gmail.com>
2026-06-13 09:34:10 +0200

    chore: auto-update 2026-06-13 09:34

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function AdminLogin() {
   const router = useRouter();
